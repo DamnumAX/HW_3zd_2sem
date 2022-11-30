4b00173d2cf99ab1e943b790a1d6473d7e8ef0d1
Filipp G <filfilet@mail.ru>
2022-11-30 18:42:26 +0300

    first commit

---
+++ dz_3zd_2sem/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число от 1 до 7:");
+int num = int.Parse(Console.ReadLine());
+
+if (num >= 1 && num <= 7)
+{
+if (num == 6 | num == 7) Console.WriteLine("Ура, сегодня выходной!");
+else Console.WriteLine("Иди на работу, сегодня не выходной!");
+}
+else Console.WriteLine("В неделе 7 дней! В следующий раз, введите число от 1 до 7");
